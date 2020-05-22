c483bbbe8b695243033473c5a225848f96e43c13
root <root@nishishailesh.tk>
2020-05-22 10:00:02 +0530

    checksum for obtained data

--- astmbi/astm_bidirectional_specific.py
+++ astmbi/astm_bidirectional_specific.py
@@ -42,6 +42,13 @@ class astms(astmg.astmg, file_mgmt):
     #elif( chr( data[ len(data) - 1 ] ).encode() == b'\x0a'):
     #easy
     elif(data[-1:] == b'\x0a'):
+      
+      if(self.get_first_inbox_file()==True):
+        fd=open(self.current_inbox_file,'wb')
+        fd.write(data)
+        fd.close()
+        self.print_to_log('File Content written:',data)
+      
       self.write_msg=b'\x06'
       self.write_set.add(self.conn[0])                      #Add in write set, for next select() to make it writable
       self.error_set=self.read_set.union(self.write_set)    #update error set      
@@ -135,7 +142,17 @@ class astms(astmg.astmg, file_mgmt):
     two_digit_checksum_string='{:X}'.format(checksum).zfill(2)
     return two_digit_checksum_string
 
+  def compare_checksum(self, received_ckecksum, calculated_checksum)
+    if(received_checksum==calculated_checksum)
+      return true
+    else
+      return false
 
+  def calculate_and_compare_checksum(self, data , calculated_checksum)
+    calculated_checksum=self.get_checksum(data)
+    received_checksum=data[-4:-2]
+    return self.compare_checksum(received_checksum,calculated_checksum)
+    
 #Main Code###############################
 #use this to device your own script
 if __name__=='__main__':
