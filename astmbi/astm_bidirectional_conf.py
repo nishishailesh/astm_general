69e61a520a0fbfdc2be3b9b89275d96f917c029a
root <biochemistrygmcs@gmail.com>
2020-06-01 18:19:31 +0530

    increased size or recve buffer

--- astmbi/astm_bidirectional_conf.py
+++ astmbi/astm_bidirectional_conf.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 astm_log_filename='/var/log/xl_1000_read.log'
 file2mysql_log_filename='/var/log/xl_1000_write.log'
-#host_address='12.207.3.240'
+host_address='12.207.3.240'
 #host_address='11.207.1.1'
 host_address=''
 host_port='2576'
